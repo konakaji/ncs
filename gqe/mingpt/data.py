e7b9c1f277c63f32dac4e37b03cb51e5fd8f55cf
knakaji <nakajijiji@gmail.com>
2023-10-11 12:24:40 -0400

    add pretrain

--- gqe/mingpt/data.py
+++ gqe/mingpt/data.py
@@ -34,6 +34,8 @@ class EnergyDataset(Dataset):
     def __init__(self, file_paths, threshold=sys.maxsize):
         tensor_x = []
         tensor_y = []
+        self.min_energy = sys.maxsize
+        self.min_indices = None
         for file_path in file_paths:
             with open(file_path) as f:
                 for l in f.readlines():
@@ -41,6 +43,9 @@ class EnergyDataset(Dataset):
                     for indices, energy in zip(data.indices, data.energies):
                         if threshold < energy:
                             continue
+                        if self.min_energy > energy:
+                            self.min_energy = energy
+                            self.min_indices = indices
                         result = [0]
                         result.extend(indices)
                         tensor_x.append(result)

f674c97bdf4fea21776d1630619ed5544d64975c
knakaji <nakajijiji@gmail.com>
2023-10-18 11:21:36 -0400

    change config

--- gptqe_task/lih_circuit.py
+++ gptqe_task/lih_circuit.py
@@ -1,17 +1,9 @@
 from experiment.experiment import LiHExperiment
-from experiment.configs import get_default_configs
 from experiment.circuit import OptimalCircuitWriter
 from gqe.gptqe.data import EnergyDataset
+from gptqe_task.lih import get_lih_configs
 
-cfg = get_default_configs()
-cfg.distances = [1.0, 1.5, 1.57, 2.0, 2.5, 3.0]
-cfg.ngates = 40
-cfg.max_iters = 500
-cfg.num_samples = 50
-cfg.n_electrons = 2
-cfg.energy_offset = 7
-cfg.nqubit = 10
-cfg.molecule_name = "LiH"
+cfg = get_lih_configs()
 
 writer = OptimalCircuitWriter(LiHExperiment())
 dataset = EnergyDataset(['checkpoints/gptqe/run_1016_12_13/trajectory_1.0.ckpt'])
--- gptqe_task/lih_pretrain.py
+++ gptqe_task/lih_pretrain.py
@@ -1,16 +1,9 @@
 from experiment.experiment import LiHExperiment
-from experiment.configs import get_default_pretrain_configs
+from gptqe_task.lih import get_lih_configs
 from torch.utils.data import DataLoader
 from gqe.mingpt.data import EnergyDataset
 
-cfg = get_default_pretrain_configs()
-cfg.ngates = 40
-cfg.num_samples = 50
-cfg.n_electrons = 2
-cfg.energy_offset = 7
-cfg.nqubit = 10
-cfg.molecule_name = "LiH"
-cfg.verbose = True
+cfg = get_lih_configs()
 
 loader = DataLoader(EnergyDataset(['checkpoints/gptqe/run_1016_12_13/trajectory_1.0.ckpt']), batch_size=50)
 

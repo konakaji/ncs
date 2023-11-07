561a68d7cc7f5c5ff7a5c665de5e516d35246bed
Mohammad <mohammd.ghazivakili@polito.it>
2023-11-07 17:59:04 -0500

    optuna code ready

--- optuna/optuna_optimization_beh2.py
+++ optuna/optuna_optimization_beh2.py
@@ -1,19 +1,23 @@
 import optuna
 from functools import partial
-from experiment.experiment import HydrogenExperiment
+from experiment.experiment import BeH2Experiment
 from experiment.configs import get_default_configs
 import sys
 
-def get_hydrogen_cfg():
+def get_beh2_configs():
     cfg = get_default_configs()
-    cfg.max_iters = 10
-    cfg.distances = [0.5, 0.6, 0.7, 0.7414, 0.8, 0.9, 1.0, 1.5, 2.0]
-    cfg.ngates = 20
-    cfg.n_electrons = 2
-    cfg.verbose = True
-    cfg.temperature = 5
-    cfg.molecule_name = "H2"
+    cfg.distances = [2.5]
+    cfg.ngates = 80
+    cfg.max_iters = 500
+    cfg.num_samples = 15
+    cfg.backward_frequency = 6
+    cfg.n_electrons = 4
+    cfg.energy_offset = 14
+    cfg.nqubit = 12
+    cfg.del_temperature = 0.1
+    cfg.molecule_name = "BeH2"
     return cfg
+
 
 
 
@@ -21,29 +25,26 @@ def get_hydrogen_cfg():
 
 def qptqe_main(
     trials,
-    molecule_name="H2",
-    n_steps=10,
+    n_steps=50,
 ):
     print("Optuna will be apply to optimize the model")
-    print(f" molecule_name {molecule_name}")
-    if molecule_name == "H2":
-        cfg = get_hydrogen_cfg()
-        cfg.distances = [cfg.distances[-2]]
-        cfg.max_iters = n_steps
-        cfg.ngates = trials.suggest_int("ngates", 10, cfg.ngates,1)
-        cfg.temperature = trials.suggest_float("temperature", cfg.temperature,20.0,step=5.5)
-        cfg.del_temperature = trials.suggest_float("del_temperature", 0.0, 1.0,step=0.05)
-        
-        cfg.embd_pdrop =  trials.suggest_float("embd_pdrop", 0.0, 0.99,step=0.01)
-        cfg.attn_pdrop =  trials.suggest_float("attn_pdrop", 0.0, 0.99,step=0.01)
-        cfg.num_samples = trials.suggest_int("num_samples", 5, 100,1) # we may need to change the bondry for this one
-        
-        energy = HydrogenExperiment().run_optuna(cfg)
+    cfg = get_beh2_configs()
+    cfg.distances = [cfg.distances[-2]]
+    cfg.max_iters = n_steps
+    cfg.ngates = trials.suggest_int("ngates", 10, cfg.ngates,1)
+    cfg.temperature = trials.suggest_float("temperature", cfg.temperature,20.0,step=5.5)
+    cfg.del_temperature = trials.suggest_float("del_temperature", 0.0, 1.0,step=0.05)
+    
+    cfg.embd_pdrop =  trials.suggest_float("embd_pdrop", 0.0, 0.99,step=0.01)
+    cfg.attn_pdrop =  trials.suggest_float("attn_pdrop", 0.0, 0.99,step=0.01)
+    cfg.num_samples = trials.suggest_int("num_samples", 5, 100,1) # we may need to change the bondry for this one
+    
+    energy = BeH2Experiment().run_optuna(cfg)
         
     return energy 
 
 if __name__ == "__main__":
-    molecule_name = str(sys.argv[1])
+    molecule_name = "BeH2"
     storage = f"sqlite:///qptqe_{molecule_name}.db"
     study_name = f"qptqe_{molecule_name}"
     study = optuna.create_study(

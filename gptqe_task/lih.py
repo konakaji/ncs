6f9730c54197fd9319abac89ee6e2546626d1418
knakaji <nakajijiji@gmail.com>
2023-11-18 11:52:13 -0500

    fix

--- gptqe_task/lih.py
+++ gptqe_task/lih.py
@@ -4,17 +4,21 @@ from experiment.configs import get_default_configs
 
 def get_lih_configs():
     cfg = get_default_configs()
-    cfg.distances = [1.0, 1.5, 1.57, 2.0, 2.5, 3.0]
+    cfg.distances = [1.0, 1.25, 1.5, 1.57, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0]
     cfg.ngates = 40
-    cfg.max_iters = 500
+    cfg.max_iters = 300
     cfg.num_samples = 50
     cfg.n_electrons = 2
     cfg.energy_offset = 7
     cfg.nqubit = 10
     cfg.del_temperature = 0.1
     cfg.molecule_name = "LiH"
+    cfg.small = True
     return cfg
 
 
 if __name__ == '__main__':
-    LiHExperiment().train(get_lih_configs())
+    cfg = get_lih_configs()
+    for seed in [0, 1, 2]:
+        cfg.seed = seed
+        LiHExperiment().train(cfg)

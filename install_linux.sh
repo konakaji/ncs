80bf21ea1876992a5739062ec3eb50c8358d45a5
Pooja Rao <porao@login26.chn>
2023-11-28 17:55:25 -0800

    updating install workflow.

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,17 @@
+cd qwrapper
+pip install -r requirements.txt
+pip install .
+cd ../qml
+pip install -r requirements.txt
+pip install .
+cd ../benchmark
+pip install -r requirements.txt
+pip uninstall jax
+pip uninstall jaxlib
+pip install .
+cd ../qswift
+pip install -r requirements.txt
+pip install .
+cd ..
 pip install -r requirements_linux.txt
-# For tequila
-# pip uninstall jax
-# pip uninstall jaxlib
 pip install autograd
